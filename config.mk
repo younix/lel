6ad84540f1b6baec45bcb45c392773dc254e290e
Hiltjo Posthuma <hiltjo@codemadness.org>
2015-11-17 21:32:08 +0100

    config.mk: introduce X11LIB and X11INC, makes porting simpler

--- config.mk
+++ config.mk
@@ -6,21 +6,22 @@ VERSION = 0.1
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+
 # includes and libs
-INCS =
-LIBS = -lc -lX11
+INCS = -I${X11INC}
+LIBS = -L${X11LIB} -lX11
+
+# flags
+CPPFLAGS = -DVERSION=\"${VERSION}\"
 
 # debug
-CFLAGS = -O0 -g -std=c99 -Wall -pedantic -DVERSION=\"${VERSION}\"
+CFLAGS = -O0 -g -std=c99 -Wall -pedantic ${INCS} ${CPPFLAGS}
 LDFLAGS = ${LIBS}
 
 # optimized
-#CFLAGS = -O2 -std=c99 -DVERSION=\"${VERSION}\"
-#LDFLAGS = -s ${LIBS}
-
-# tcc
-#CC = tcc
-#CFLAGS = -DVERSION=\"${VERSION}\"
+#CFLAGS = -O2 -std=c99 -DVERSION=\"${VERSION}\" ${INCS} ${CPPFLAGS}
 #LDFLAGS = -s ${LIBS}
 
 # compiler and linker
